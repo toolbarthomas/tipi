41f1ddc067ef8fdfa27e93cf98c33472e86934b9
Thomas van der Velde <contact@toolbarthomas.net>
2016-06-06 18:04:26 +0200

    Fix: Updated Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,131 +1,47 @@
 module.exports = function(grunt) {
 
 	require('jit-grunt')(grunt, {
+		cachebreaker : 'grunt-cache-breaker',
 		replace: 'grunt-text-replace'
 	});
 
-	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		clean: {
-			dist: {
-				src : [
-					'dist/*'
-				]
-			},
-			build: {
-				src : [
-					'build/'
-				]
-			}
-		},
+		developmentPath: 'dev',
+		productionPath: 'dist',
+		modulePath: 'git_submodules',
 
-		copy: {
-			dist: {
-				expand: true,
-				cwd: 'dev/',
-				src: [
-					'assets/webfonts/**',
-					'assets/css/**',
-					'assets/js/**',
-				],
-				dest: 'dist/'
-			},
-			build: {
-				expand: true,
-				cwd: 'dist/',
-				src: [
-					'**.html',
-					'assets/js/**',
-					'assets/img/**',
-					'assets/css/**',
-					'assets/webfonts/**'
-				],
-				dest: 'build/'
-			},
-			img: {
-				expand: true,
-				cwd: 'dev/',
-				src: [
-					'assets/img/**/*.{png,jpg,gif}',
-				],
-				dest: 'dist/'
-			},
-			svg: {
-				expand: true,
-				cwd: 'dev/',
-				src: [
-					'assets/img/**/*.svg',
-				],
-				dest: 'dist/'
-			},
-			bower_components: {
-				expand: true,
-				cwd: 'bower_components/',
-				src: [
-					'svg-localstorage/svg-localstorage.js',
-				],
-				dest: 'dist/assets/js/lib/'
-
-			}
-		},
-
-		zetzer: {
-			options: {
-				partials: "dev/inc/partials/",
-				templates: "dev/inc/templates/"
-			},
-			bower_components: {
-				files: [{
-					expand: true,
-					cwd: 'git_submodules/',
-					src: '**/*.html',
-					dest: 'dist/pages/',
-					ext: '.html',
-					flatten: false
-				}]
-			},
-			getting_started: {
-				files: [{
-					expand: true,
-					cwd: 'dev/',
-					src: '*.html',
-					dest: 'dist/',
-					ext: '.html',
-					flatten: false
-				}]
-			}
-		},
-
-
-		//Compile the sass files with Compass
 		compass: {
-			dist: {
+			options: {
+				cssDir: 'assets/css',
+				sassDir: 'assets/sass',
+				imagesDir: 'assets/img',
+				javascriptsDir: 'assets/js',
+				fontsDir: 'assets/webfonts',
+				relativeAssets: true,
+				quiet: true,
+				require : [
+					'sass-globbing',
+				]
+			},
+			development: {
 				options: {
-					basePath: 'dev/',
-					cssDir: 'assets/css',
-					sassDir: 'assets/sass',
-					imagesDir: 'assets/img',
-					javascriptsDir: 'assets/js',
-					fontsDir: 'assets/webfonts',
+					basePath: '<%= developmentPath %>/',
 					environment: 'development',
 					outputStyle: 'expanded',
-					relativeAssets: true,
 					noLineComments: false,
-					quiet: true,
-					sourcemap: true,
-					require : [
-						'sass-globbing',
-					]
+					sourcemap: true
 				}
 			}
 		},
 
 		svgstore: {
-			dist: {
+			development: {
 				files: {
-					'dev/assets/img/layout/svg-sprite.svg': ['dev/assets/img/layout/svg/*.svg']
+					'<%= productionPath %>/assets/img/layout/svg-sprite.svg': [
+						'<%= developmentPath %>/assets/img/layout/svg/**/*.svg'
+					]
 				},
 				options: {
 					cleanup: true,
@@ -137,104 +53,123 @@ module.exports = function(grunt) {
 		},
 
 		concat: {
-			dist: {
-				options: {
-					separator: ';'
-				},
-				files: {
-					'dist/assets/js/main.js' : [
-						'dev/assets/js/main.js'
-					],
-					'dist/assets/js/tipi/tipi.components.js' : [
-						'bower_components/**/tipi.*.js'
-					]
-				}
+			options: {
+				separator: ';',
+				sourceMap: true
+			},
+			modules : {
+				src: (function () {
+					var cwd = "<%= modulePath %>/";
+					var files = [
+						"**/tipi.*.js",
+					];
+
+					files = files.map(function (file) {
+						return cwd + file;
+					});
+
+					return files;
+				}()),
+				dest: '<%= productionPath %>/assets/js/lib/tipi/tipi.js',
 			}
 		},
 
-		concurrent: {
-			compile: [
-				'zetzer',
-				'compass:dist',
-				'svgstore:dist',
-				'concat:dist',
-			],
-			minify: [
-				'cssmin:build',
-				'svgmin:build',
-				'imageoptim:build',
-				'uglify:build',
-				'replace:assets',
-				'cacheBust'
-
-			]
-		},
-
-		cssmin: {
-			build: {
-				files: {
-					'build/assets/css/tipi.min.css': ['dist/assets/css/tipi.css']
-				}
-			}
-		},
-
-		svgmin: {
-			build: {
-				options: {
-					plugins: [
-						{ removeViewBox: false },
-						{ removeEmptyAttrs: false }
-					]
-				},
+		zetzer: {
+			options: {
+				partials: "<%= developmentPath %>/inc/partials/",
+				templates: "<%= developmentPath %>/inc/templates/"
+			},
+			development: {
 				files: [{
 					expand: true,
-					cwd: 'dev/assets/img/layout/svg/',
-					src: ['*.svg'],
-					dest: 'svgs/'
+					cwd: '<%= developmentPath %>/',
+					src: '*.html',
+					dest: '<%= productionPath %>/',
+					ext: '.html',
+					flatten: false
+				}]
+			},
+			modules: {
+				files: [{
+					expand: true,
+					cwd: '<%= modulePath %>/',
+					src: '**/tipi.*.html',
+					dest: '<%= productionPath %>/pages/',
+					ext: '.html',
+					flatten: false
 				}]
 			}
 		},
 
-		imageoptim: {
-			build: {
-				src: ['build/assets/img/layout'],
-				options : {
-					quitAfter : true
-				}
+		clean: {
+			development: {
+				src: [
+					'<%= productionPath %>'
+				]
 			}
 		},
 
-		//Minify Javascript for production
-		uglify: {
-			dist: {
-				options: {
-					compress : {
-						drop_console : true
-					},
-					sourceMap : false
-				},
-				files: {
-					'dist/assets/js/lib/**.js': 'dist/assets/js/lib/**.min.js'
-				}
+		copy: {
+			development: {
+				expand: true,
+				cwd: '<%= developmentPath %>/',
+				src: [
+					'assets/**'
+				],
+				dest: '<%= productionPath %>/'
 			},
-			build: {
-				options : {
-					compress : {
-						drop_console : true,
-					},
-					sourceMap : false,
+			img: {
+				expand: true,
+				cwd: '<%= developmentPath %>/',
+				src: [
+					'assets/**/*.{.png,jpg,gif,svg}'
+				],
+				dest: '<%= productionPath %>/'
+			}
+		},
+
+		combine_mq: {
+			production: {
+				expand: true,
+				cwd: '<%= developmentPath %>/assets/css/',
+				src: ['**/*.css', '!**/*.min.css'],
+				dest: '<%= developmentPath %>/assets/css/'
+			}
+		},
+
+		cssmin: {
+			production: {
+				files: [{
+					expand: true,
+					cwd: '<%= developmentPath %>/assets/css',
+					src: ['*.css', '!*.min.css'],
+					dest: '<%= productionPath %>/assets/css',
+					ext: '.min.css'
+				}]
+			}
+		},
+
+		image: {
+			production: {
+				options: {
+					jpegRecompress: true,
+					jpegoptim: true,
+					mozjpeg: true,
+					svgo: true
 				},
-				files: {
-					'build/assets/js/tipi/tipi.components.min.js': 'dist/assets/js/tipi/tipi.components.js',
-					'build/assets/js/main.min.js': 'dist/assets/js/main.js',
-				}
+				files: [{
+					expand: true,
+					cwd: '<%= productionPath %>/assets/img/',
+					src: ['**/*.{png,jpg,gif,svg}'],
+					dest: '<%= productionPath %>/assets/img/'
+				}]
 			}
 		},
 
 		replace: {
-			assets: {
-				src: ['build/**.html'],
-				dest: 'build/',
+			production: {
+				src: ['<%= productionPath%>/**/*.html'],
+				dest: '<%= productionPath%>/',
 				replacements: [
 					{
 						from: 'tipi.css',
@@ -245,48 +180,112 @@ module.exports = function(grunt) {
 						to: 'main.min.js'
 					},
 					{
-						from: 'tipi.components.js',
-						to: 'tipi.components.min.js'
+						from: 'tipi.js',
+						to: 'tipi.min.js'
 					}
-				]
-			},
-			svgSprite: {
-				src: ['dist/**.html'],
-				dest: 'dist/',
-				replacements: [
-					{
-						from: 'svgRev',
-						to: '<%= grunt.template.today("yyyymmddHHMMss") %>'
-					},
 				]
 			}
 		},
 
-		cacheBust: {
-			options: {
-				expand: true,
-				baseDir: 'build/',
-				queryString: true
-			},
-			css: {
+		cachebreaker: {
+			production: {
 				options: {
-					assets: ['assets/css/**'],
+					match: [
+						'cached',
+						'css/*/tipi.min.css',
+						'js/*/tipi.min.js'
+					]
 				},
-				src: ['build/**.html']
+				files: {
+					src: [
+						'<%= productionPath %>/**/*.html'
+					]
+				}
+			}
+		},
+
+		watch: {
+			options: {
+				interrupt: true,
+			},
+			scss: {
+				files: [
+					'<%= developmentPath %>/assets/sass/**/*.scss',
+					'<%= modulePath %>/**/*.scss',
+
+					'!**/node_modules/**',
+				],
+				tasks: [
+					'compass:development',
+					'svgstore:development'
+				],
+				options: {
+					spawn : true
+				}
+			},
+			img: {
+				files: [
+					'<%= developmentPath %>/assets/img/**/*.{png,jpg,gif,svg}',
+
+					'!**/node_modules/**'
+				],
+				tasks: [
+					'newer:copy:img'
+				],
+				options : {
+					event: ['added', 'deleted']
+				}
+			},
+			svg: {
+				files: [
+					'<%= developmentPath %>/**/*.svg'
+				],
+				tasks: [
+					'svgstore:development'
+				]
 			},
 			js: {
-				options: {
-					assets: ['assets/js/**'],
-				},
-				src: ['build/**.html']
+				files: [
+					'<%= modulePath %>/**/tipi.*.js'
+				],
+				tasks: [
+					'concat:modules'
+				]
+			},
+			html: {
+				files: [
+					'<%= developmentPath %>/**/*.html',
+					'<%= modulePath %>/tipi.*/*.html'
+				],
+				tasks: [
+					'zetzer:development',
+					'zetzer:modules',
+				]
+			},
+			reload: {
+				files: [
+					'<%= productionPath %>/**/*.css'
+				],
+				options : {
+					livereload : true,
+					livereloadOnError: false
+				}
+			},
+			config: {
+				files : [
+					'Gruntfile.js'
+				],
+				options : {
+					reload : true
+				}
 			}
 		},
 
 		connect: {
-			server: {
+			development: {
 				options: {
 					port: 8000,
-					base: 'dist/',
+					base: '<%= productionPath %>',
 					livereload: true,
 					open: {
 						target: 'http://localhost:8000'
@@ -295,149 +294,50 @@ module.exports = function(grunt) {
 			}
 		},
 
-		watch: {
-			html: {
-				files: [
-					'dev/*.html',
-					'dev/inc/**/*.html',
-					'dev/docs/**/*.html',
-					'git_submodules/**/*.html',
-
-					'!**/dist/**',
-					'!**/build/**',
-					'!**/node_modules/**',
-				],
-				tasks: ['zetzer'],
-
-			},
-			scss: {
-				files: [
-					'git_submodules/**/*.scss',
-					'dev/assets/sass/**/*.scss',
-
-					'!**/dist/**',
-					'!**/build/**',
-					'!**/node_modules/**',
-				],
-				tasks: [
-					'compass:dist',
-					'copy:dist'
-				],
-				options: {
-					spawn : true,
-				}
-			},
-			js: {
-				files: [
-					'dev/assets/js/**/*.js',
-
-					'!**/dist/**',
-					'!**/build/**',
-					'!**/node_modules/**',
-				],
-				tasks: [
-					'concat:dist'
-				],
-				options: {
-					spawn : false,
-					interrupt: true
-				}
-			},
-			img: {
-				files: [
-					'dev/assets/img/**/*.{png,jpg,gif}',
-
-					'!**/dist/**',
-					'!**/build/**',
-					'!**/node_modules/**',
-				],
-				tasks: [
-					'newer:copy:img'
-				],
-				options : {
-					event: ['added', 'deleted']
-				},
-			},
-			svg: {
-				files: [
-					'dev/assets/img/**/*.svg',
-				],
-				tasks: [
-					'svgstore:dist',
-					'newer:copy:svg',
-					'zetzer',
-					'replace:svgSprite'
-				],
-				options : {
-					event: ['added', 'deleted']
-				}
-
-			},
-			reload : {
-				files: [
-					'dist/assets/css/**/*.css',
-					'dist/**/*.html',
-				],
-				options : {
-					livereload : true,
-					livereloadOnError: false
-				},
-			},
-			config : {
-				files : [
-					'Gruntfile.js'
-				],
-				options : {
-					reload : true
-				}
-
-			}
+		concurrent: {
+			development: [
+				'compass:development',
+				'svgstore:development',
+				'concat:modules',
+				'zetzer:development',
+				'zetzer:modules',
+			],
+			production: [
+				'combine_mq:production',
+				'cssmin:production',
+				'newer:image:production'
+			]
 		}
 	});
 
 	grunt.registerTask(
 		'default', [
-			'clean:dist',
-			'concurrent:compile',
-			'replace:svgSprite',
-			'copy:img',
-			'copy:svg',
-			'copy:dist',
+			'clean:development',
+			'concurrent:development',
+			'copy:development'
 		]
 	);
 
-	grunt.registerTask(
-		'build', [
-			'clean:dist',
-			'clean:build',
-			'concurrent:compile',
-			'replace:svgSprite',
-			'copy:dist',
-			'copy:img',
-			'copy:svg',
-			'copy:build',
-			'concurrent:minify',
-		]
-	);
 
 	grunt.registerTask(
 		'serve', [
-			'clean:dist',
-			'concurrent:compile',
-			'replace:svgSprite',
-			'copy:dist',
-			'copy:img',
-			'copy:svg',
-			'copy:bower_components',
-			'connect:server',
-			'watch',
+			'clean:development',
+			'concurrent:development',
+			'copy:development',
+			'connect:development',
+			'watch'
 		]
 	);
 
+
 	grunt.registerTask(
-		'cleanup', [
-			'clean:dist',
-			'clean:build',
+		'build', [
+			'clean:development',
+			'concurrent:development',
+			'copy:development',
+			'concurrent:production',
+			'replace:production',
+			'cachebreaker:production'
 		]
 	);
 };
