b677e213068df1e33d6a7fcb0f0d30f5abed77bf
Thomas van der Velde <contact@toolbarthomas.net>
2016-12-06 16:47:37 +0100

    Fix: Cleanup

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,3 @@
 $(function() {
-	setTwins();
-	setUnifiedCheckbox();
-	setUnifiedRadio();
-	setUnifiedSelect();
-	setUploader();
-	setTeaser();
-	setTopBar();
-	setTouchMenu();
-	setBigBannerCarousel();
-	setAccordion();
-	setTarzan();
-
-	$('#haha').on({
-		click : function (event) {
-			event.preventDefault();
-			$(this).remove();
-			$(document).trigger('tipi.UPDATE');
-		}
-	})
 
 });
