4e6414bae9fe3065f86f67080fa9766fcdbaeff6
Thomas van der Velde <contact@toolbarthomas.net>
2017-09-26 13:57:16 +0200

    Use correct task within correct file

--- gulp-tasks/connect.js
+++ gulp-tasks/connect.js
@@ -1,48 +1,8 @@
 module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
     return function (callback) {
-        PLUGINS.watch([
-            process.env.SRC + '/assets/**/*',
-        ], function () {
-            GULP.start('copy');
-        }
-        );
-
-        PLUGINS.watch([
-            process.env.SRC + '/assets/**/*.scss',
-            PACKAGES + '/**/*.scss',
-        ], function () {
-            GULP.start('stylesheets');
-        }
-        );
-
-        PLUGINS.watch([
-            process.env.SRC + '/assets/img/layout/svg-sprite/**/*.svg',
-        ], function () {
-            GULP.start('svgstore');
-        }
-        );
-
-        PLUGINS.watch([
-            process.env.SRC + '/assets/img/layout/sprite/**/*.png',
-        ], function () {
-            GULP.start('spritesmith');
-        }
-        );
-
-        PLUGINS.watch([
-            process.env.SRC + '/assets/**/*.js',
-            PACKAGES + '/**/tipi.*.js'
-        ], function () {
-            GULP.start('javascripts');
-        }
-        );
-
-        PLUGINS.watch([
-            process.env.SRC + '/**/*.twig',
-            PACKAGES + '/*tipi*/*.twig',
-        ], function () {
-            GULP.start('pages');
-        }
-        );
+        return PLUGINS.connect.server({
+            root: DEST,
+            livereload: true
+        });
     }
 }
