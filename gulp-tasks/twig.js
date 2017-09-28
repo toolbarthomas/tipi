f26360ceac7ac549f4f70055d1db58aa9198d012
Thomas van der Velde <contact@toolbarthomas.net>
2017-09-28 09:36:25 +0200

    Typo

--- gulp-tasks/twig.js
+++ gulp-tasks/twig.js
@@ -21,6 +21,20 @@ module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
                 process.env.MODULES_PATH + '/*tipi*/**/*.twig'
             ])
             .pipe(PLUGINS.plumber())
+            .pipe(PLUGINS.data(function (file) {
+                var data = 'data.json';
+
+                var path = NODE_MODULES.path.relative(file.cwd, file.path);
+                var extension = NODE_MODULES.path.basename(path);
+
+                var data = path.replace(extension, 'data.json');
+
+                // Check if data.json exists
+                if (!NODE_MODULES.fse.existsSync(data)) {
+                    return {};
+                }
+                return JSON.parse(NODE_MODULES.fse.readFileSync(data));
+            }))
             .pipe(PLUGINS.twig(twig_options))
             .pipe(PLUGINS.faker())
             .pipe(GULP.dest(process.env.DEST + '/packages'));
